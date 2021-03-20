c9aefd108827d8f1a39b6cfc6727caf0c8e40627
as0387 <as0387@naver.com>
2021-03-20 13:59:08 +0900

    환경세팅 완료

---
+++ src/main/java/com/dongs/itsmine/ItsmineApplication.java
@@ -0,0 +1,13 @@
+package com.dongs.itsmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsmineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsmineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongs/itsmine/ItsmineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongs.itsmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsmineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fcbb13cd1bc8a4e6dcc917c7b728145ef561e236
as0387 <as0387@naver.com>
2021-03-29 17:09:47 +0900

    전동적인 로그인 방법 스프링 시큐리티 시작 전

--- src/main/java/com/dongs/itsmine/repository/UserRepository.java
+++ src/main/java/com/dongs/itsmine/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.dongs.itsmine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dongs.itsmine.model.User;
 
@@ -8,5 +9,13 @@ import com.dongs.itsmine.model.User;
 //자동으로 bean 등록이 된다.
 //@Repository //생략 가능
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	//JPA Naming 전략
+	//네이밍 전략에 맞춰 함수를 만들면 SELECT * FROM user WHERE username= ?1 AND password=?2; 요런쿼리가 동작
+	User findByUsernameAndPassword(String username, String password);
+	
+	/*
+	 * @Query(value="SELECT * FROM user WHERE username= ?1 AND password=?2",nativeQuery = true)
+	 * User login(String username, String password);
+	 * 요방법도 있음
+	 */
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
 		$("#btn-save").on("click", ()=>{//this를 바인딩하기위해.
 			this.save();
 		});
+		$("#btn-login").on("click", ()=>{//this를 바인딩하기위해.
+			this.login();
+		});
 	},
 	save: function() {
 		//alert('user의 save함수 호출됨.');
@@ -22,11 +25,33 @@ let index = {
 		dataType: "json" //요청을 서버로해서 응답이 왔을때 기본적으로 모든것이 String (생긴게 json 이라면) => JavaScript 오브젝트로 변경
 		}).done(function(resp){
 			alert("회원가입이 완료되었습니다.");
-			console.log(resp);
+			location.href = "/itsmine";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); // ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+	},
+	login: function() {
+		//alert('user의 save함수 호출됨.');
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val()
+		};
+		
+		//console.log(data);
+		//ajax호출시 default가 비동기 호출
+		$.ajax({
+		type: "POST",
+		url: "/itsmine/api/user/login",
+		data: JSON.stringify(data),//http body 데이터
+		contentType: "application/json; chaset=utf-8", //body 데이터가 어떤 타입인지
+		dataType: "json" //요청을 서버로해서 응답이 왔을때 기본적으로 모든것이 String (생긴게 json 이라면) => JavaScript 오브젝트로 변경
+		}).done(function(resp){
+			alert("로그인이 완료되었습니다.");
 			location.href = "/itsmine";
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		}); // ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
 	}
+	
 }
 index.init();
